58b01ace421d895cc50c19de17977622454d9a86
Samuel Munyi <samunyi90@gmail.com>
2023-04-14 10:50:50 +0300

    Updates

--- src/main/java/com/example/fxlearning/App.java
+++ src/main/java/com/example/fxlearning/App.java
@@ -1,2 +1,19 @@
-package com.example.fxlearning;public class App {
+package com.example.fxlearning;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Using FX Bi-directional Binding");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/main/java/com/example/fxlearning/AppController.java
+++ src/main/java/com/example/fxlearning/AppController.java
@@ -1,2 +1,38 @@
-package com.example.fxlearning;public class AppController {
+package com.example.fxlearning;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.text.NumberFormat;
+import java.util.ResourceBundle;
+
+public class AppController implements Initializable {
+    @FXML
+    private Slider slider;
+
+    @FXML
+    private TextField textField;
+
+    private static final double INITIAL_VALUE = 50;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        slider.setValue(INITIAL_VALUE);
+        textField.setText(String.valueOf(INITIAL_VALUE));
+
+        textField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null && !newValue.isEmpty()) {
+                try {
+                    textField.textProperty().bindBidirectional(slider.valueProperty(), NumberFormat.getNumberInstance());
+                    textField.setStyle("-fx-background-color: white;"); // Reset the background color
+                } catch (Exception e) {
+                    textField.setStyle("-fx-background-color: pink;"); // Set the background color to indicate an error
+                }
+            }
+        });
+
+    }
 }
+
