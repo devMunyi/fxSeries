fb1f5bf39dc66845faa77673704f28b19c16e83b
Samuel Munyi <samunyi90@gmail.com>
2023-04-19 15:14:19 +0300

    Using fx radiobutton

--- src/main/java/com/example/fxlearning/App.java
+++ src/main/java/com/example/fxlearning/App.java
@@ -2,20 +2,20 @@ package com.example.fxlearning;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("app.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
+        Scene scene = new Scene(root);
 
-        stage.setTitle("Using FX Checkboxes");
         stage.setScene(scene);
+        stage.setTitle("Using FX Radio Button");
         stage.show();
     }
-
 
     public static void main(String[] args){
         launch(args);
--- src/main/java/com/example/fxlearning/AppController.java
+++ src/main/java/com/example/fxlearning/AppController.java
@@ -1,55 +1,25 @@
 package com.example.fxlearning;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 
 public class AppController {
+    @FXML private Label myLabel;
 
-    @FXML
-    private CheckBox cb1;
+    @FXML private RadioButton rb1;
 
-    @FXML
-    private CheckBox cb2;
+    @FXML private RadioButton rb2;
 
-    @FXML
-    private CheckBox cb3;
-
-    @FXML
-    private CheckBox cb4;
-
-    @FXML
-    private Label labelList;
-
-    @FXML
-    private Label labelCount;
-
-    public void handleCheckboxEvent(ActionEvent event){
-        int count = 0;
+    public void selectRadioBtn(){
         String message = "";
 
-        if(cb1.isSelected()){
-            count++;
-            message += cb1.getText() + "\n";
+        if(rb1.isSelected()){
+            message = rb1.getText();
+        } else if (rb2.isSelected()) {
+            message = rb2.getText();
         }
 
-        if(cb2.isSelected()){
-            count++;
-            message += cb2.getText() + "\n";
-        }
-
-        if(cb3.isSelected()){
-            count++;
-            message += cb3.getText() + "\n";
-        }
-
-        if(cb4.isSelected()){
-            count++;
-            message += cb4.getText() + "\n";
-        }
-
-        labelList.setText(message);
-        labelCount.setText("Selected items : " + count);
+        myLabel.setText(message);
     }
 }
